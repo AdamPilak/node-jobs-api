9cd88b4d469685be017310b11189a46a44672d12
Unisuu <pilak.adam@gmail.com>
2024-06-23 12:48:03 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+require("dotenv").config()
+require("express-async-errors")
+
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+const express = require("express")
+const app = express()
+
+const connectDB = require("./db/connect")
+const authenticateUser = require("./middleware/authentication")
+
+// routers
+const { authRouter, jobsRouter } = require("./routes")
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000,
+		max: 100,
+	})
+)
+app.use([express.json(), helmet(), cors(), xss()])
+
+// routes
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", authenticateUser, jobsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI)
+		app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
---
+++ routes/jobs.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+
+const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require("../controllers/jobs")
+
+router.get("/", getAllJobs)
+router.get("/:id", getJob)
+router.post("/", createJob)
+router.patch("/:id", updateJob)
+router.delete("/:id", deleteJob)
+
+module.exports = router
